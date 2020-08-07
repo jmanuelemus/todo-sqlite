f08fc22ca2559dfe8007e98684b35a56fb0c519f
Juan Manuel Lemus <jmanuelemus@gmail.com>
2020-08-07 10:36:33 -0600

    Add Users schema

--- database/migration.sql
+++ database/migration.sql
@@ -14,3 +14,26 @@ CREATE TABLE todo_people
 );
 
 CREATE INDEX todo_people_name ON todo_people ("family_name", "given_name");
+
+# ---
+
+CREATE TABLE todo_users
+(
+    "_id"                  INTEGER        NOT NULL
+        CONSTRAINT todo_user
+            PRIMARY KEY autoincrement,
+
+    "_type"                VARCHAR  ( 32),
+    "_obj"        UNSIGNED INTEGER,
+    "verified_at"          DATETIME,
+    "name"                 VARCHAR  ( 64) NOT NULL,
+    "password"             VARCHAR  (255) NOT NULL,
+    "_created_at"          DATETIME       NOT NULL,
+    "_updated_at"          DATETIME,
+    "_deleted_at"          DATETIME
+);
+
+CREATE UNIQUE INDEX todo_users_name ON todo_users ("name");
+
+CREATE INDEX todo_users_type     ON todo_users ("_type");
+CREATE INDEX todo_users_obj_type ON todo_users ("_obj", "_type");
--- database/seeds.sql
+++ database/seeds.sql
@@ -2,3 +2,10 @@ INSERT INTO todo_people(
     verified_at, given_name, family_name, alternate_name, _created_at
 
 ) VALUES (datetime('now'), 'Juan Manuel', 'Lemus Ponciano', 'Juanma', datetime('now'));
+
+# ---
+
+INSERT INTO todo_users (
+    _type, _obj, verified_at, name, password, _created_at
+
+) VALUES ('Person', 1, datetime('now'), 'jmanuelemus', '$2y$10$aM3M4xfwniFpmvLPI5ztmeT6.VwxU9UYfBN.WVLpjcv3HK/ci9lvq', datetime('now'));
