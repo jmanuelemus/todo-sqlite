502b19f72abc02fcd8192fa4248b7b118113a575
Juan Manuel Lemus <jmanuelemus@gmail.com>
2020-08-07 12:41:29 -0600

    Add AdministrativeDivisions schema

--- database/migration.sql
+++ database/migration.sql
@@ -1,3 +1,36 @@
+CREATE TABLE todo_administrative_divisions
+(
+    "_id"                      INTEGER         NOT NULL
+        CONSTRAINT todo_administrative_division
+            PRIMARY KEY autoincrement,
+
+    "_sup"            UNSIGNED INTEGER
+        CONSTRAINT todo_administrative_division_geo_within
+            REFERENCES todo_administrative_divisions
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "_lft"                     INTEGER,
+    "_rgt"                     INTEGER,
+    "_type"                    VARCHAR  ( 32) NOT NULL,
+    "_alias"                   TEXT,
+    "identifier"               VARCHAR  ( 16),
+    "postal_code"              VARCHAR  ( 16),
+    "name"                     VARCHAR  (255) NOT NULL,
+    "_created_at"              DATETIME       NOT NULL,
+    "_updated_at"              DATETIME,
+    "_deleted_at"              DATETIME
+);
+
+CREATE UNIQUE INDEX todo_administrative_divisions_identifier  ON todo_administrative_divisions ("identifier");
+CREATE UNIQUE INDEX todo_administrative_divisions_name        ON todo_administrative_divisions ("_sup", "name");
+CREATE UNIQUE INDEX todo_administrative_divisions_postal_code ON todo_administrative_divisions ("_sup", "postal_code");
+
+CREATE INDEX todo_administrative_divisions_sup             ON todo_administrative_divisions ("_sup");
+CREATE INDEX todo_administrative_divisions_type            ON todo_administrative_divisions ("_type");
+
+# ---
+
 CREATE TABLE todo_people
 (
     "_id"            INTEGER        NOT NULL
