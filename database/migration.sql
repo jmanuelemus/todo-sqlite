4f0c387596c13fc1543c7fc776ce58eea0e05c81
Juan Manuel Lemus <jmanuelemus@gmail.com>
2020-08-07 14:12:15 -0600

    Add Addresses schema

--- database/migration.sql
+++ database/migration.sql
@@ -70,3 +70,66 @@ CREATE UNIQUE INDEX todo_users_name ON todo_users ("name");
 
 CREATE INDEX todo_users_type     ON todo_users ("_type");
 CREATE INDEX todo_users_obj_type ON todo_users ("_obj", "_type");
+
+# ---
+
+CREATE TABLE todo_addresses
+(
+    "_id"                       INTEGER        NOT NULL
+        CONSTRAINT todo_address
+            PRIMARY KEY autoincrement,
+
+    "_eq"              UNSIGNED INTEGER
+        CONSTRAINT todo_address_same_as
+            REFERENCES todo_addresses
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "_sup"             UNSIGNED INTEGER        NOT NULL
+        CONSTRAINT todo_address_sup
+            REFERENCES todo_users
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "_type"                     VARCHAR  ( 32),
+    "verified_at"               DATETIME,
+    "additional_type"           VARCHAR  ( 32),
+    "address_country"           VARCHAR  (  2) NOT NULL
+        CONSTRAINT todo_address_country
+            REFERENCES todo_administrative_divisions (identifier)
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "address_region"            VARCHAR  (  8) NOT NULL
+        CONSTRAINT todo_address_region
+            REFERENCES todo_administrative_divisions (identifier)
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "address_locality"          VARCHAR  ( 16) NOT NULL
+        CONSTRAINT todo_address_locality
+            REFERENCES todo_administrative_divisions (identifier)
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "address_location"          VARCHAR  ( 16)
+        CONSTRAINT todo_address_location
+            REFERENCES todo_administrative_divisions (identifier)
+                ON UPDATE restrict
+                ON DELETE restrict,
+
+    "street_address"            VARCHAR  (255) NOT NULL,
+    "street_name"               VARCHAR  ( 48),
+    "street_number"             VARCHAR  ( 24),
+    "_created_at"               DATETIME       NOT NULL,
+    "_updated_at"               DATETIME,
+    "_deleted_at"               DATETIME
+);
+
+CREATE INDEX todo_addresses_eq               ON todo_addresses ("_eq");
+CREATE INDEX todo_addresses_uid              ON todo_addresses ("_sup");
+CREATE INDEX todo_addresses_type             ON todo_addresses ("_type");
+CREATE INDEX todo_addresses_address_locality ON todo_addresses ("address_country", "address_region", "address_locality");
+CREATE INDEX todo_addresses_address_location ON todo_addresses ("address_locality", "address_location");
+CREATE INDEX todo_addresses_street_address   ON todo_addresses ("address_locality", "street_address");
+CREATE INDEX todo_addresses_street_name      ON todo_addresses ("address_locality", "street_number");
